7b363effb4f5260fd2e6f0455c8fbe668fe658d1
FadeScripting <79726377+Sheepy9999@users.noreply.github.com>
2023-06-25 14:40:56 +0200

    Create example.lua

---
+++ example/example.lua
@@ -0,0 +1,321 @@
+Fade
+fadewashere
+Online
+
+Alec — Today at 12:20
+yea
+runs smoothly
+Fade — Today at 12:21
+because my fluxus crashes for no reason
+Alec — Today at 12:21
+ah
+Fade — Today at 12:21
+valyse: fixed
+fluxus: im sorry to announce that our injecting is broken
+Alec — Today at 12:21
+LMAO
+FRFR
+XDD
+Fade — Today at 12:21
+its actually true btw..
+Alec — Today at 12:21
+:laugh~5:
+Fade — Today at 12:22
+less go
+Image
+Alec — Today at 12:22
+see?
+it is still good
+even if it never broke for me
+lol
+Fade — Today at 12:22
+besides this
+Image
+Alec — Today at 12:22
+eh, that is fixed for me
+lol
+Fade — Today at 12:22
+oh
+Alec — Today at 12:23
+runs very smooth for me
+i love it
+Fade — Today at 12:23
+same for rn
+Alec — Today at 12:23
+where is rn
+i need to steal rn's valyse
+to give it to u
+fr
+Fade — Today at 12:24
+casual
+Image
+Alec — Today at 12:24
+fr
+Fade — Today at 12:24
+autofarm doesn't work
+Alec — Today at 12:24
+it does
+fr
+Fade — Today at 12:24
+not on exploding car
+Alec — Today at 12:24
+ah
+Fade — Today at 12:25
+but on exploding car i can just let tinytask repeat this lemme screenshare
+Alec — Today at 12:25
+coems
+Fade — Today at 12:25
+are you watching?
+Alec — Today at 12:25
+ye
+lol
+sounds like my pc
+Fade — Today at 12:26
+LOL
+Alec — Today at 12:26
+when i open shadersa
+yea
+like that
+Fade — Today at 12:26
+.
+Alec — Today at 12:26
+but
+10x
+times
+louder
+Fade — Today at 12:26
+how to hit 125T fr fr
+u screenshare
+i wanna watch
+lol
+Alec — Today at 12:27
+jumpscare
+fr
+Fade — Today at 12:29
+"teleport to vechile
+Alec — Today at 12:29
+oh
+brb
+Fade — Today at 12:44
+https://www.youtube.com/watch?v=_UsrVGxN8-E
+YouTube
+CrispyGo
+I Targeted Her With INVISIBLE GUN In Da Hood (banned)
+Image
+Image
+Image
+Image
+Image
+Fade — Today at 12:59
+Airport
+Fade — Today at 13:06
+24
+free farm
+maybe ill stop it faster
+idk
+Alec — Today at 13:09
+Image
+Fade — Today at 13:26
+https://www.roblox.com/game-pass/195442499
+2 - Roblox
+Roblox is a global platform that brings people together through play.
+Alec — Today at 13:27
+Image
+Fade — Today at 13:32
+SpawnLocations
+Alec — Today at 13:33
+Image
+Fade — Today at 13:34
+Image
+Image
+Image
+Image
+Image
+Image
+https://www.youtube.com/watch?v=UmQDj32kuMg
+YouTube
+Airrack
+I Spent $100,000 On Facebook Marketplace!
+Image
+Alec — Today at 13:38
+Attachment file type: unknown
+The_Fallen_Forest.rbxm
+1.27 MB
+Fade — Today at 13:43
+@Alec
+where did u go?
+Alec — Today at 13:43
+wait
+me get source
+of img printer
+and autobuild
+Clyde
+BOT
+ — Today at 13:44
+It appears you’ve been by yourself in this call for more than 3 minutes. The bandwidth patrol has asked me to disconnect you to save bandwidth. That stuff doesn’t grow on trees!
+Only you can see this • Dismiss message
+Fade — Today at 13:46
+cmon just join
+idc about the source code
+Fade
+ started a call.
+ — Today at 13:46
+Alec — Today at 13:50
+Image
+Alec — Today at 13:58
+https://raw.githubusercontent.com/titusj3026/MyFiles/main/RobloxScripts/BuildABoat/Script/script.lua
+Fade — Today at 14:13
+Image
+Image
+Alec — Today at 14:14
+suvrcial
+How to get fucky wucky with Rei frfr
+Image
+thumbnail
+Alec — Today at 14:25
+https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwj5h8WCtd7_AhXKR_EDHXH8DsgQFnoECBYQAQ&url=https%3A%2F%2Fv3rmillion.net%2Fshowthread.php%3Fpid%3D7858033&usg=AOvVaw0Drvtj4Ybq547B319wOKeO&opi=89978449
+Fade — Today at 14:26
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/un-named%20backups/jans", true))()
+
+local Window = Library:CreateWindow("thing")
+
+local folder = Window:AddFolder('stuff')
+folder:AddToggle({text = "This is a toggle", callback = function(v) print(v) end})
+folder:AddButton({text = 'button', callback = function() print('button clicked') end})
+folder:AddSlider({text = 'slider', min = 1, max = 100, callback = function(v) print(v) end})
+folder:AddList({text = 'option', values = {'yo', 'yes'}, callback = function(v) print(v) end})
+folder:AddBox({text = 'text box', callback = function(v) print(v) end})
+folder:AddColor({text = 'color', callback = function(v) print(v) end})
+Alec — Today at 14:33
+Elerium [IMGUI]
+Image
+WE GOT IT
+CHAT WILL BLOW THE FUCK UP
+YAY
+:catparty:
+FUCK
+BOOT UP BREAD TICKETS MAFAKA
+AAA
+BOOT UP BREAD TICKETS MAFAKA
+BOOT UP BREAD TICKETS MAFAKA
+BOOT UP BREAD TICKETS MAFAKA
+BOOT UP BREAD TICKETS MAFAKA
+BOOT UP BREAD TICKETS MAFAKA
+BOOT UP BREAD TICKETS MAFAKA
+BOOT UP BREAD TICKETS MAFAKABOOT UP BREAD TICKETS MAFAKA
+BOOT UP BREAD TICKETS MAFAKA
+@Fade
+@Fade
+@Fade
+@Fade
+Fade — Today at 14:38
+local FadeUI = loadstring(game:HttpGet("https://raw.githubusercontent.com/Sheepy9999/FadeUI/main/source/main.lua", true))()
+
+local Window = FadeUI.new("FadeUI UI", "v1.0.0", 4370345701)
+
+local Tab1 = Window.Tab("Tab 1", 6026568198)
+local Folder = Tab1.Folder("Options", "A bunch of options you can use")
+Expand
+message.txt
+6 KB
+Alec — Today at 14:38
+@Fade
+@Fade
+@Fade
+﻿
+local FadeUI = loadstring(game:HttpGet("https://raw.githubusercontent.com/Sheepy9999/FadeUI/main/source/main.lua", true))()
+
+local Window = FadeUI.new("FadeUI UI", "v1.0.0", 4370345701)
+
+local Tab1 = Window.Tab("Tab 1", 6026568198)
+local Folder = Tab1.Folder("Options", "A bunch of options you can use")
+Folder.Button("Button", function()
+print("Button Clicked")
+end)
+Folder.Switch("Switch", function(Status)
+print("Switch Triggered: " .. tostring(Status))
+end)
+Folder.Toggle("Toggle", function(Status)
+print("Toggle Triggered: " .. tostring(Status))
+end)
+Folder.Toggle("Toggle", function(Status)
+print("Toggle Triggered: " .. tostring(Status))
+end)
+Folder.TextBox("Textbox", "Placeholder", function(Text)
+print("TextBox Triggered: " .. Text)
+end)
+Tab1.Folder("Lipsum Expanded", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eu mollis urna, quis feugiat tellus. Integer ut ligula sodales, sodales ipsum ut, imperdiet ipsum. In aliquet quam et venenatis pulvinar. Nullam fermentum porta felis sit amet interdum. Sed tristique fringilla mollis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam quis tempus mauris, nec ultrices metus. Suspendisse mi urna, accumsan at nisi a, tristique porta libero. Integer lobortis elementum lacus cursus consectetur. Morbi mauris ante, posuere at malesuada et, tristique non ipsum. Proin vitae purus pretium, convallis est vitae, dignissim leo. Praesent nec felis vitae.")
+
+local Cheat = Tab1.Cheat("Options", "A bunch of options you can use", function(Status)
+print("Cheat Triggered: " .. tostring(Status))
+end)
+Cheat.Button("Button", function()
+print("Button Clicked")
+end)
+Cheat.Switch("Switch", function(Status)
+print("Switch Triggered: " .. tostring(Status))
+end)
+Cheat.Toggle("Toggle", function(Status)
+print("Toggle Triggered: " .. tostring(Status))
+end)
+Cheat.Toggle("Toggle", function(Status)
+print("Toggle Triggered: " .. tostring(Status))
+end)
+Cheat.TextBox("Textbox", "Placeholder", function(Text)
+print("TextBox Triggered: " .. Text)
+end)
+
+Tab1.Cheat("Lipsum Expanded", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eu mollis urna, quis feugiat tellus. Integer ut ligula sodales, sodales ipsum ut, imperdiet ipsum. In aliquet quam et venenatis pulvinar. Nullam fermentum porta felis sit amet interdum. Sed tristique fringilla mollis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam quis tempus mauris, nec ultrices metus. Suspendisse mi urna, accumsan at nisi a, tristique porta libero. Integer lobortis elementum lacus cursus consectetur. Morbi mauris ante, posuere at malesuada et, tristique non ipsum. Proin vitae purus pretium, convallis est vitae, dignissim leo. Praesent nec felis vitae.", function(Status)
+print("Cheat Triggered: " .. tostring(Status))
+end)
+
+-- Tab 2 --
+local Tab2 = Window.Tab("Tab 2", 6022668945)
+local Folder = Tab2.Folder("Options", "A bunch of options you can use")
+Folder.Button("Button", function()
+print("Button Clicked")
+end)
+Folder.Switch("Switch", function(Status)
+print("Switch Triggered: " .. tostring(Status))
+end)
+Folder.Toggle("Toggle", function(Status)
+print("Toggle Triggered: " .. tostring(Status))
+end)
+Folder.Toggle("Toggle", function(Status)
+print("Toggle Triggered: " .. tostring(Status))
+end)
+Folder.TextBox("Textbox", "Placeholder", function(Text)
+print("TextBox Triggered: " .. Text)
+end)
+Tab2.Folder("Lipsum Expanded", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eu mollis urna, quis feugiat tellus. Integer ut ligula sodales, sodales ipsum ut, imperdiet ipsum. In aliquet quam et venenatis pulvinar. Nullam fermentum porta felis sit amet interdum. Sed tristique fringilla mollis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam quis tempus mauris, nec ultrices metus. Suspendisse mi urna, accumsan at nisi a, tristique porta libero. Integer lobortis elementum lacus cursus consectetur. Morbi mauris ante, posuere at malesuada et, tristique non ipsum. Proin vitae purus pretium, convallis est vitae, dignissim leo. Praesent nec felis vitae.")
+
+local Cheat = Tab2.Cheat("Options", "A bunch of options you can use", function(Status)
+print("Cheat Triggered: " .. tostring(Status))
+end)
+Cheat.Button("Button", function()
+print("Button Clicked")
+end)
+Cheat.Switch("Switch", function(Status)
+print("Switch Triggered: " .. tostring(Status))
+end)
+Cheat.Toggle("Toggle", function(Status)
+print("Toggle Triggered: " .. tostring(Status))
+end)
+Cheat.Toggle("Toggle", function(Status)
+print("Toggle Triggered: " .. tostring(Status))
+end)
+Cheat.TextBox("Textbox", "Placeholder", function(Text)
+print("TextBox Triggered: " .. Text)
+end)
+
+Tab2.Cheat("Lipsum Expanded", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eu mollis urna, quis feugiat tellus. Integer ut ligula sodales, sodales ipsum ut, imperdiet ipsum. In aliquet quam et venenatis pulvinar. Nullam fermentum porta felis sit amet interdum. Sed tristique fringilla mollis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam quis tempus mauris, nec ultrices metus. Suspendisse mi urna, accumsan at nisi a, tristique porta libero. Integer lobortis elementum lacus cursus consectetur. Morbi mauris ante, posuere at malesuada et, tristique non ipsum. Proin vitae purus pretium, convallis est vitae, dignissim leo. Praesent nec felis vitae.", function(Status)
+print("Cheat Triggered: " .. tostring(Status))
+end)
+
+game:GetService("UserInputService").InputBegan:Connect(function(Input)
+if Input.KeyCode == Enum.KeyCode.F then
+Window:Toggle()
+end
+end)
+message.txt
+6 KB
